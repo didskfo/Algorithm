3b27e113c358cca4a58dbf98a411fb2168ea9db9
didskfo <101495452+didskfo@users.noreply.github.com>
2022-10-28 23:43:09 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 1.67 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        int cnt = 0;
+        for(int i = 0; i < str.length; i++){
+            if(str[i].equals(" ")){
+               cnt = 0; 
+            }else if(cnt % 2 == 0){
+                str[i] = str[i].toUpperCase();
+                cnt++;
+            }else{
+                str[i] = str[i].toLowerCase();
+                cnt++;
+            }
+            answer += str[i];
+        }
+        return answer;
+    }
+}
