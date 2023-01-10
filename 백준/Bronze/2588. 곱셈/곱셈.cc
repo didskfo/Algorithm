b7365b0198907e360db9a6da07145f8daaf07609
didskfo <101495452+didskfo@users.noreply.github.com>
2023-01-10 14:33:05 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    char b[4];
+    cin >> a;
+    cin >> b;
+    cout << a * (b[2] - '0') << endl;
+    cout << a * (b[1] - '0') << endl;
+    cout << a * (b[0] - '0') << endl;
+    cout << a * atoi(b) << endl;
+    return 0;
+}
