ec429e625f131e9b7a4cb512214d8af300516ecc
didskfo <101495452+didskfo@users.noreply.github.com>
2023-01-11 12:27:32 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        int b, c;
+        cin >> b;
+        cin >> c;
+        cout << b+c << endl;
+    }
+    return 0;
+}
