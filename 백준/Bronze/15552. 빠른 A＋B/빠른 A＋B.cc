55b5cfbb195a9c7369ec59be819f242ded3c82ed
didskfo <101495452+didskfo@users.noreply.github.com>
2023-01-13 00:48:51 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a;
+        cin >> b;
+        cout << a+b << "\n";
+    }
+    return 0;
+}
