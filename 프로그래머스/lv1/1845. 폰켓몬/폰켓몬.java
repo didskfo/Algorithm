9b2eb6767d9b56bfcc51c6c8fa817a1f8d00569b
didskfo <101495452+didskfo@users.noreply.github.com>
2023-02-02 14:42:16 +0900

    [level 1] Title: 폰켓몬, Time: 2.00 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!arr.contains(nums[i])) {
+                arr.add(nums[i]);
+            }
+        }
+        if (arr.size() >= nums.length/2) {
+            answer = nums.length/2;
+        } else {
+            answer = arr.size();
+        }
+        return answer;
+    }
+}
