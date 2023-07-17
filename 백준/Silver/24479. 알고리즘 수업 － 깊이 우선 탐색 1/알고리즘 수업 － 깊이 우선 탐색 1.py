041908c30c936ad8f717c975986a831ef6559816
didskfo <101495452+didskfo@users.noreply.github.com>
2023-07-17 14:01:56 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 572 ms, Memory: 61556 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys 
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for i in range(n+1)]
+visit = [False for i in range(n+1)]
+cnt = [0 for i in range(n+1)]
+num = 1
+stack = [r]
+
+for i in range(m):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+    
+for i in graph:
+    i.sort(reverse=True)
+    
+while stack:
+    v = stack.pop()
+    if visit[v]:
+        continue
+    cnt[v] = num
+    num += 1
+    visit[v] = True
+    for i in graph[v]:
+        if visit[i] == False:
+            stack.append(i)
+
+print(*cnt[1:], sep='\n')
