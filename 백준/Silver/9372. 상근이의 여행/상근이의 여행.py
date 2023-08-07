b4a1a2866964b372ce3c00bf693846b1b1680768
didskfo <101495452+didskfo@users.noreply.github.com>
2023-08-07 14:00:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 172 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,10 @@
+import sys 
+input = sys.stdin.readline
+from collections import deque
+
+t = int(input())
+for _ in range(t):
+    N, M = map(int, input().split())
+    for q in range(M):
+        a, b = map(int, input().split())
+    print(N-1)
