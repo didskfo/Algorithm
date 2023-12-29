8754044765b9968aa40e999ba00b00b8db7e2597
didskfo <101495452+didskfo@users.noreply.github.com>
2023-12-29 22:52:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.65 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,19 @@
+# def solution(participant, completion):
+#     answer = ''
+#     dic = {}
+#     for i in set(participant):
+#         dic[i] = 0
+#     for i in participant:
+#         dic[i] += 1
+#     for i in completion:
+#         dic[i] -= 1
+#     for i in dic:
+#         if dic[i] != 0:
+#             answer += i
+#     return answer
+
+from collections import Counter
+
 def solution(participant, completion):
-    answer = ''
-    dic = {}
-    for i in set(participant):
-        dic[i] = 0
-    for i in participant:
-        dic[i] += 1
-    for i in completion:
-        dic[i] -= 1
-    for i in dic:
-        if dic[i] != 0:
-            answer += i
-    return answer
+    answer = list(Counter(participant)-Counter(completion))
+    return answer[0]
