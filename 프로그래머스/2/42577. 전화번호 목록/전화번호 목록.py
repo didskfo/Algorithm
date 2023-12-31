3b403f7c29483dfb9a7ecafda96bfafb1832a114
didskfo <101495452+didskfo@users.noreply.github.com>
2023-12-31 17:17:30 +0900

    [level 2] Title: 전화번호 목록, Time: 201.19 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,27 @@
+# def solution(phone_book):
+#     answer = True
+#     phone_book.sort()
+#     for i in range(len(phone_book)-1):
+#         if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+#             return False
+#     return answer
+
+# def solution(phoneBook):
+#     phoneBook = sorted(phoneBook)
+
+#     for p1, p2 in zip(phoneBook, phoneBook[1:]):
+#         if p2.startswith(p1):
+#             return False
+#     return True
+
 def solution(phone_book):
-    answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
-            return False
-    return answer
+    dic = {}
+    for phone_number in phone_book:
+        dic[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in dic and temp != phone_number:
+                return False
+    return True
