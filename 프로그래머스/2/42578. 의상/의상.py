630d0839290d6a8a34c3c95f11b3d0d7eb37b3dd
didskfo <101495452+didskfo@users.noreply.github.com>
2024-01-01 20:12:15 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,19 @@
+# from collections import defaultdict
+# def solution(clothes):
+#     answer = 1
+#     dic = defaultdict(list)
+#     for i, j in clothes:
+#         dic[j].append(i)
+#     for i in dic:
+#         answer *= len(dic[i])+1
+#     return answer-1
+
 from collections import defaultdict
 def solution(clothes):
     answer = 1
-    dic = defaultdict(list)
+    dic = defaultdict(int)
     for i, j in clothes:
-        dic[j].append(i)
+        dic[j] += 1
     for i in dic:
-        answer *= len(dic[i])+1
-    answer -= 1
-    return answer
+        answer *= dic[i]+1
+    return answer-1
