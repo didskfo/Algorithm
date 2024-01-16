ec4c17d2c0b5ad610570fd74e8d00fb9aa01bce8
didskfo <101495452+didskfo@users.noreply.github.com>
2024-01-16 14:35:18 +0900

    Update 더 맵게.py

--- 프로그래머스/Lv.2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/Lv.2/42626. 더 맵게/더 맵게.py
@@ -13,6 +13,7 @@ def solution(scoville, K):
         answer += 1
     if scoville[0] < K:
         return -1
+    return answer
     # if len_scoville == 2:
     #     if scoville[0]+scoville[1]*2 >= K:
     #         return 1
@@ -26,4 +27,4 @@ def solution(scoville, K):
     #     answer += 1
     #     if answer >= len_scoville-1:
     #         return -1
-    return answer
+    # return answer
