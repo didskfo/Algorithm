f5fcee18c96a92423e66e0c8dbaca726874d7109
didskfo <101495452+didskfo@users.noreply.github.com>
2024-01-18 13:14:56 +0900

    Update 다리를 지나는 트럭.py

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -18,23 +18,3 @@ def solution(bridge_length, weight, truck_weights):
         answer += 1
         
     return answer+len(bridge)
-
-# from collections import deque
-
-# def solution(bridge_length, weight, truck_weights):
-#     time = 1
-#     bridge = 
-#     truck_weights = deque(truck_weights)
-#     sum_trucks = 0
-
-#     while truck_weights:
-#         sum_trucks += truck_weights[0]
-#         if sum_trucks <= weight:
-#             bridge.append(truck_weights.popleft())
-#         else:
-#             sum_trucks -= truck_weights[0]
-#             bridge.append(0)
-#         sum_trucks -= bridge.popleft()
-#         time += 1
-
-#     return time + len(bridge)
