51f6764a7d3a6e28e79d17eed9be3f7637a34b1e
didskfo <101495452+didskfo@users.noreply.github.com>
2024-04-04 12:32:32 +0900

    Update 평균 구하기.java

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public double solution(int[] arr) {
-        double answer = Arrays.stream(arr).average().orElse(0);
-        return answer;
+        return Arrays.stream(arr).average().orElse(0);
     }
 }
