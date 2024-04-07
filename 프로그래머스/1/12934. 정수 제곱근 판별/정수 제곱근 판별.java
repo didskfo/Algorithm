cc7bb345e799b98a83fec025314098d6d43d834a
didskfo <101495452+didskfo@users.noreply.github.com>
2024-04-08 00:18:01 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,7 @@
+class Solution {
+    public long solution(long n) {
+        if (Math.sqrt(n) == (int)Math.sqrt(n)) {
+            return (long)Math.pow(Math.sqrt(n)+1,2);
+        } else return -1;
+    }
+}
