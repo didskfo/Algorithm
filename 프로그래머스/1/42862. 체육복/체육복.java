d21a28bb9f9f1aa926e2e4ba2e560906e4e8bb79
didskfo <101495452+didskfo@users.noreply.github.com>
2024-05-10 12:29:55 +0900

    [level 1] Title: 체육복, Time: 1.70 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] stu = new int[n];
+        Arrays.fill(stu, 1);
+        
+        for (int l : lost) stu[l-1] -= 1 ;
+        
+        for (int r : reserve) stu[r-1] += 1;
+        
+        for (int i = 0; i < n; i++) {
+            if (stu[i] == 0) {
+                if (i-1 >= 0 && stu[i-1] > 1) {
+                    stu[i] = 1;
+                    stu[i-1] -= 1;
+                    continue;
+                }
+                if (i+1 < n && stu[i+1] > 1) {
+                    stu[i] = 1;
+                    stu[i+1] -= 1;
+                    continue;
+                }
+            }
+        }
+                
+        List<Integer> list = Arrays.stream(stu).boxed().collect(Collectors.toList());        
+        int cnt_0 = Collections.frequency(list, 0);
+        return n-cnt_0;
+    }
+}
