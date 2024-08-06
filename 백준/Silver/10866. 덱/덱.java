402e6518aee84eb4af092fd60a5c773edf33d983
didskfo <101495452+didskfo@users.noreply.github.com>
2024-08-06 12:18:07 +0900

    [Silver IV] Title: 덱, Time: 380 ms, Memory: 20296 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> que = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            if (input[0].equals("push_front")) que.addFirst(Integer.parseInt(input[1]));
+            else if (input[0].equals("push_back")) que.add(Integer.parseInt(input[1]));
+            else if (input[0].equals("pop_front")) {
+                if (!que.isEmpty()) System.out.println(que.pollFirst());
+                else System.out.println(-1);
+            } else if (input[0].equals("pop_back")) {
+                if (!que.isEmpty()) System.out.println(que.pollLast());
+                else System.out.println(-1);
+            } else if (input[0].equals("size")) System.out.println(que.size());
+            else if (input[0].equals("empty")) {
+                if (que.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else if (input[0].equals("front")) {
+                if (!que.isEmpty()) System.out.println(que.peekFirst());
+                else System.out.println(-1);
+            } else if (input[0].equals("back")) {
+                if (!que.isEmpty()) System.out.println(que.peekLast());
+                else System.out.println(-1);
+            }
+        }
+    }
+}
