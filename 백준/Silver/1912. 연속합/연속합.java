58778459beccbef1c0e25b005dcf5884392c4b24
didskfo <101495452+didskfo@users.noreply.github.com>
2024-08-10 15:32:35 +0900

    [Silver II] Title: 연속합, Time: 216 ms, Memory: 23692 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            dp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i-1]+dp[i], dp[i]);
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
+    }
+}
