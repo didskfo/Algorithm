b5c0585e7533fb8e9f4af31e7e424a23e5e8a8ea
didskfo <101495452+didskfo@users.noreply.github.com>
2024-09-28 23:55:33 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N, r, c;
+    static int cnt = 0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        find(r, c, (int)Math.pow(2, N));
+        System.out.println(cnt);
+    }
+
+    static void find(int x, int y, int size) {
+        if (size == 1) return;
+        if (x<size/2 && y<size/2) {
+            find(x, y, size/2);
+        } else if (x<size/2 && y>=size/2) {
+            cnt += size*size/4;
+            find(x, y-size/2, size/2);
+        } else if (x>=size/2 && y<size/2) {
+            cnt += (size*size/4)*2;
+            find(x-size/2, y, size/2);
+        } else {
+            cnt += (size*size/4)*3;
+            find(x-size/2, y-size/2, size/2);
+        }
+    }
+}
