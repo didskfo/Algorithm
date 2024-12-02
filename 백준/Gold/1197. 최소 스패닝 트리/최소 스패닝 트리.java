1756ad7dd52545da716eb18001379be192691b87
didskfo <101495452+didskfo@users.noreply.github.com>
2024-12-02 13:32:27 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 460 ms, Memory: 48084 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,32 +2,79 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-class Edge implements Comparable<Edge> {
+class Node implements Comparable<Node> {
     int u, v, c;
 
-    public Edge(int u, int v, int c) {
+    public Node(int u, int v, int c) {
         this.u = u;
         this.v = v;
         this.c = c;
     }
 
     @Override
-    public int compareTo(Edge other) {
+    public int compareTo(Node other) {
         if (this.c != other.c) {
             return Integer.compare(this.c, other.c);
-        } else if (this.u != other.u){
-            return Integer.compare(this.u, other.u);
-        } else {
-            return Integer.compare(this.v, other.v);
         }
+        return 0;
     }
 }
-public class Main {
 
-    static int[] parent;
+public class Main {
+    public static int n, m, u, v, c;
+    public static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n+1];
+        for (int i = 0; i < n+1; i++) {
+            parent[i] = i;
+        }
+
+        ArrayList<Node> nodeList = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            u = Integer.parseInt(st.nextToken());
+            v = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+
+            nodeList.add(new Node(u, v, c));
+        }
+
+        PriorityQueue<Node> prique = new PriorityQueue<>(nodeList);
+
+        int total = 0;
+        int cnt = 0;
+
+        while (!prique.isEmpty()) {
+            Node node = prique.poll();
+
+            int ur = find(node.u);
+            int vr = find(node.v);
+
+            if (ur != vr) {
+                total += node.c;
+                parent[ur] = vr;
+                cnt++;
+            }
+
+            if (cnt > n-2) {
+                break;
+            }
+        }
+
+        bf.close();
+
+        System.out.println(total);
+
+    }
 
     public static int find(int u) {
         int r = u;
@@ -35,63 +82,12 @@ public class Main {
             r = parent[r];
         }
 
-        while (parent[u] != u) {
-            int tmp = parent[u];
+        while(parent[u] != u) {
+            int tmp_u = parent[u];
             parent[u] = r;
-            u = tmp;
+            u = tmp_u;
         }
 
         return r;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        parent = new int[n+1];
-
-        for (int i = 0; i < n+1; i++) {
-            parent[i] = i;
-        }
-
-        ArrayList<Edge> edgeList = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            edgeList.add(new Edge(u, v, c));
-        }
-
-
-        PriorityQueue<Edge> edges = new PriorityQueue<>(edgeList);
-
-        int total_cost = 0;
-        int num_edges = 0;
-
-        while (!edges.isEmpty()) {
-            Edge e = edges.poll();
-
-            int ur = find(e.u);
-            int vr = find(e.v);
-
-            if (ur != vr) {
-                total_cost += e.c;
-                num_edges += 1;
-                parent[ur] = vr;
-            }
-
-            if (num_edges >= n-1) {
-                break;
-            }
-        }
-
-        br.close();
-
-        System.out.println(total_cost);
     }
 }
