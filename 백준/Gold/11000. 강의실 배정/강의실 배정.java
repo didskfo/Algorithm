9715479b49113e6e5b9f5255215f3f997d6a9eb0
didskfo <101495452+didskfo@users.noreply.github.com>
2025-01-09 13:27:32 +0900

    [Gold V] Title: 강의실 배정, Time: 604 ms, Memory: 71136 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<int[]> que = new PriorityQueue<>((q1, q2) -> Integer.compare(q1[0], q2[0]));
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int s = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            que.offer(new int[]{s, t});
+        }
+
+        // 강의 종료 시간 우선순위 큐
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            if (!pq.isEmpty() && cur[0] >= pq.peek()) pq.poll(); // 앞 강의 종료시간보다 현 강의 시작시간이 뒤라면 강의실 재사용 가능
+            pq.offer(cur[1]); // 현 강의 종료시간 추가
+        }
+
+        System.out.println(pq.size());
+    }
+}
