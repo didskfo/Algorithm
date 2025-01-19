89bebb14761793a8eb8efdd8a6906268f7e4f576
didskfo <101495452+didskfo@users.noreply.github.com>
2025-01-19 14:34:30 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            arr[i] = new int[]{Integer.parseInt(input[0]),Integer.parseInt(input[1]),Integer.parseInt(input[2])};
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st;
+            int[][] arr = new int[N][3];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
+                arr[i][2] = Integer.parseInt(st.nextToken());
+            }
+            int[][] dp = new int[N][3];
+            dp[0] = Arrays.copyOf(arr[0], 3);
+            for (int i = 1; i < N; i++) {
+                dp[i][0] = arr[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+                dp[i][1] = arr[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+                dp[i][2] = arr[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+            }
+            System.out.println(Arrays.stream(dp[N - 1]).min().getAsInt());
         }
-        int[][] dp = new int[n][3];
-        for (int i = 0; i < 3; i++) {
-            dp[0][i] = arr[0][i];
-        }
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
-            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
-        }
-        System.out.println(Math.min(Math.min(dp[n-1][0], dp[n-1][1]),dp[n-1][2]));
     }
 }
