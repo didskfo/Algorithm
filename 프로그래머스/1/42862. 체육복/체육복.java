a3ca5c07f969fa3d1ee20c273f269d8d8b8fad17
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-05 16:15:56 +0900

    [level 1] Title: 체육복, Time: 5.24 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,33 +1,27 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
         int[] stu = new int[n];
         Arrays.fill(stu, 1);
-        
-        for (int l : lost) stu[l-1] -= 1 ;
-        
-        for (int r : reserve) stu[r-1] += 1;
-        
+        for (int l : lost) {
+            stu[l-1] -= 1;
+        }
+        for (int r : reserve) {
+            stu[r-1] += 1;
+        }
         for (int i = 0; i < n; i++) {
             if (stu[i] == 0) {
-                if (i-1 >= 0 && stu[i-1] > 1) {
+                if (i != 0 && stu[i-1] > 1) {
                     stu[i] = 1;
                     stu[i-1] -= 1;
-                    continue;
-                }
-                if (i+1 < n && stu[i+1] > 1) {
+                } else if (i < n-1 && stu[i+1] > 1) {
                     stu[i] = 1;
                     stu[i+1] -= 1;
-                    continue;
                 }
             }
         }
-                
-        for (int i : stu) {
-            if (i == 0) n--; 
-        }
-        return n;
+        return n - (int)Arrays.stream(stu).filter(i -> i == 0).count();
     }
 }
