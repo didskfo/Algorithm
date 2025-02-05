d0aac284cd2c81f5349dfd99182845c1b7fd11d2
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-05 17:10:29 +0900

    [level 2] Title: 기능개발, Time: 4.26 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,27 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        int cnt;
-        int length = progresses.length;
-        
-        while (length!=0) {
-            for (int i = 0; i < length; i++) {
-                progresses[i] += speeds[i];
+        int day = 0; 
+        int[] res = new int[100];
+        for (int i = 0; i < progresses.length; i++) {
+            while (progresses[i] + day*speeds[i] < 100) {
+                day++;
             }
-            cnt = 0;
-            while (length!=0 && progresses[0] >= 100) {
-                cnt += 1;
-                progresses = Arrays.copyOfRange(progresses, 1, length);
-                speeds = Arrays.copyOfRange(speeds, 1, length); 
-                length -= 1;
-            }
-            if (cnt > 0) arr.add(cnt);
-            
+            res[day]++;
         }
-        int[] answer = arr.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();;
-        return answer;
+        
+        return Arrays.stream(res).filter(i -> i != 0).toArray();
     }
 }
