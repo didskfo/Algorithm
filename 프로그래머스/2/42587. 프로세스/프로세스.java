5122583904e4da91aadd2ece01668485dcc0ec94
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-06 14:37:22 +0900

    [level 2] Title: 프로세스, Time: 2.93 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,30 +1,25 @@
 import java.util.*;
-
 class Pair {
     int lo;
     int pri;
-
     public Pair(int lo, int pri) {
         this.lo = lo;
         this.pri = pri;
     }
 }
-
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        int lo = 0;
-        boolean flag = true;
         Deque<Pair> que = new ArrayDeque<>();
-        for (int p : priorities) {
-            que.add(new Pair(lo++, p));
+        for (int i = 0; i < priorities.length; i++) {
+            que.offer(new Pair(i, priorities[i]));
         }
-        while (que.size() != 0) {
+        int answer = 0;
+        while (!que.isEmpty()) {
+            boolean flag = true;
             Pair cur = que.poll();
-            flag = true;
             for (Pair p : que) {
                 if (cur.pri < p.pri) {
-                    que.add(cur);
+                    que.offer(cur);
                     flag = false;
                     break;
                 }
@@ -32,9 +27,8 @@ class Solution {
             if (flag) {
                 answer++;
                 if (cur.lo == location) return answer;
-            };
+            }
         }
-        
         return answer;
     }
 }
