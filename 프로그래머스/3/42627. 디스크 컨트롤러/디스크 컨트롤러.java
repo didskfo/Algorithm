822cf218449db92ace73056617c0d19aeb108906
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-14 15:46:49 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 8.46 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,24 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
+        PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         int answer = 0;
-        int time = 0;
         int idx = 0;
-        int before = -1; 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        
+        int before = -1;
+        int time = 0;
         while (idx < jobs.length) {
             for (int[] job : jobs) {
-                if (before < job[0] && job[0] <= time) {
-                    pq.add(job);
+                if (job[0] <= time && job[0] > before) {
+                    que.add(job);
                 }
             }
-            
-            if (!pq.isEmpty()) {
-                int[] cur = pq.poll();
+            if (!que.isEmpty()) {
+                int[] cur = que.poll();
                 before = time;
                 time += cur[1];
-                answer += time - cur[0];
+                answer += time-cur[0];
                 idx++;
             } else {
                 time++;
