3d64d6ea01a9ee2133f9491f513327446af3ca87
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-15 22:43:11 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String text = br.readLine();
+            String pattern = br.readLine();
+            int answer = 0;
+            for (int i = 0; i <= text.length()-pattern.length(); i++) {
+                boolean check = true;
+                for (int j = 0; j < pattern.length(); j++) {
+                    if (text.charAt(i+j) != pattern.charAt(j)) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    answer++;
+                    i += pattern.length()-1;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
