dc7e43ac6842d3ef66ae1a716e6ec65ca12748a3
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-20 14:05:30 +0900

    [level 2] Title: 구명보트, Time: 26.18 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        Arrays.sort(people);
         Deque<Integer> que = new ArrayDeque<>();
+        Arrays.sort(people);
         for (int p : people) {
             que.add(p);
         }
@@ -13,7 +13,7 @@ class Solution {
                 answer++;
                 break;
             }
-            if (que.getFirst() + que.getLast() <= limit) {
+            if (que.peek() + que.peekLast() <= limit) {
                 que.poll();
                 que.pollLast();
             } else {
