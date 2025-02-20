309a1d9b96356f99a39414bb74bed26ee285d08f
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-20 14:22:50 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT board_id, writer_id, title, price, case status when 'sale' then '판매중' when 'reserved' then '예약중' when 'done' then '거래완료' end status
+from used_goods_board
+where created_date = '2022-10-05'
+order by 1 desc;
