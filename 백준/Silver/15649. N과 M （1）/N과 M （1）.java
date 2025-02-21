0f45221f1ddc4f5c4fc0c29cfd6f370122c2b3c3
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-21 23:44:40 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 23160 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
-    public static int n, m;
-    public static int[] selected;
-    public static boolean[] visit;
+    static int n, m;
+    static boolean[] visit;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-
-        selected = new int[m];
-        visit = new boolean[n+1];
-
-        permutation(0);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            visit = new boolean[n+1];
+            selected = new int[m];
+            backTrack(0);
+            System.out.println(sb);
+        }
     }
 
-    public static void permutation(int depth) {
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(selected[i] + " ");
+    static void backTrack(int cnt) {
+        if (cnt == m) {
+            for (int a : selected) {
+                sb.append(a).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
-
         for (int i = 1; i <= n; i++) {
             if (!visit[i]) {
-                selected[depth] = i;
                 visit[i] = true;
-                permutation(depth + 1);
+                selected[cnt] = i;
+                backTrack(cnt+1);
                 visit[i] = false;
             }
         }
