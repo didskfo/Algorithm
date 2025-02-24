9bd40df755a99d2817222441cef15228155953a8
didskfo <101495452+didskfo@users.noreply.github.com>
2025-02-24 14:14:46 +0900

    [level 3] Title: 단속카메라, Time: 7.37 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,20 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        ArrayList<int[]> list = new ArrayList<>();
-        for (int[] route : routes){
-            list.add(route);
-        }
-        Collections.sort(list, (l1 , l2) -> Integer.compare(l1[1], l2[1]));
-        while(!list.isEmpty()){
-            int end = list.get(0)[1];
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i)[0] <= end) {
-                    list.remove(i);
-                    i--;
-                }
+        Arrays.sort(routes, (r1, r2) -> Integer.compare(r1[1], r2[1]));
+        int end = Integer.MIN_VALUE;
+        for (int[] route : routes) {
+            if (end < route[0]) {
+                answer++;
+                end = route[1];
             }
-            answer++;
         }
         return answer;
     }
