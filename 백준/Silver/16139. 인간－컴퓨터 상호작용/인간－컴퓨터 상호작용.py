4e35427ccdf96332a5f3439539513c282b2580f9
Narae Yang <101495452+didskfo@users.noreply.github.com>
2025-08-25 21:07:50 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 216 ms, Memory: 30840 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,22 +1,15 @@
 import sys 
-input = sys.stdin.readline
+input = sys.stdin.readline 
 
-S = input().strip()
-q = int(input())
-
-dp = [[0 for i in range(26)] for _ in range(len(S))]
-dp[0][ord(S[0])-97] = 1
-
-for i in range(1, len(S)):
-    dp[i][ord(S[i])-97] = 1
-    for j in range(26):
-        dp[i][j] += dp[i-1][j]
-
-for _ in range(q):
-    arr = input().split()
-    if int(arr[1])>0:
-        ans = dp[int(arr[2])][ord(arr[0]) - 97] - dp[int(arr[1]) - 1][ord(arr[0]) - 97]
-    else:
-        ans = dp[int(arr[2])][ord(arr[0]) - 97]
-    print(ans)
-    
+string = input()
+n = int(input())
+for _ in range(n):
+    lst = list(map(str, input().split()))
+    al = lst[0]
+    l = int(lst[1])
+    r = int(lst[2])
+    cnt = 0
+    for i in range(l, r+1):
+        if al == string[i]:
+            cnt += 1
+    print(cnt)
