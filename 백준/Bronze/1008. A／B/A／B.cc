18c07d96475a1b53ad888b76a9db1ae76b5a45b0
Narae Yang <101495452+didskfo@users.noreply.github.com>
2025-08-26 22:21:06 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,12 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     double a, b;
-    cin >> a;
-    cin >> b;
+    cin >> a >> b;
     cout.precision(10);
-    cout << a / b << endl;
-    return 0;
+    cout << a/b;
 }
