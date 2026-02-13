0f0bec4ab17fc6a1190559b05ef9b1ebcd535a82
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-13 14:28:42 +0900

    [level 1] Title: 모의고사, Time: 3.01 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] p1 = {1, 2, 3, 4, 5};
-        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int a = 0;
-        int b = 0; 
-        int c = 0;
-        
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int sa = 0;
+        int sb = 0;
+        int sc = 0;
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == p1[i%5]) a++;
-            if (answers[i] == p2[i%8]) b++;
-            if (answers[i] == p3[i%10]) c++;
+            int an = answers[i];
+            if (an == a[i%5]) sa++;
+            if (an == b[i%8]) sb++;
+            if (an == c[i%10]) sc++;
         }
-        ArrayList<Integer> ans = new ArrayList<>();
-        int max = Math.max(Math.max(a, b), c);
-        if (max == a) ans.add(1);
-        if (max == b) ans.add(2);
-        if (max == c) ans.add(3);
-        
-        return ans.stream().mapToInt(i -> i).toArray();
+        int max = Math.max(sa, Math.max(sb, sc));
+        ArrayList<Integer> answer = new ArrayList<>();
+        if (max == sa) answer.add(1);
+        if (max == sb) answer.add(2);
+        if (max == sc) answer.add(3);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
