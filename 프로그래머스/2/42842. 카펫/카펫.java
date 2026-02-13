8ea67061be7fd868223e996963eac77d58d8c78a
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-13 16:16:09 +0900

    [level 2] Title: 카펫, Time: 8.88 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,22 @@
 class Solution {
-    public int[] solution(int brown, int yellow) {     
-        for (int i = 1; i <= yellow; i++) {
-            if (yellow % i == 0) {
-                int wid = yellow/i + 2;
-                int hei = i + 2;
-                if (wid * hei == brown + yellow) {
-                    return new int[]{wid, hei};
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        for (int i = 1; i <= Math.sqrt(yellow); i++) {
+            System.out.println(i);
+            if (yellow % i == 0) { 
+                int yHeight = i;
+                int yWidth = yellow / i;
+                
+                int carpetWidth = yWidth + 2;
+                int carpetHeight = yHeight + 2;
+                
+                if ((carpetWidth * 2) + (yHeight * 2) == brown) {
+                    answer[0] = carpetWidth;
+                    answer[1] = carpetHeight;
+                    return answer; 
                 }
             }
         }
-        return new int[]{0, 0};
+        return answer;
     }
 }
