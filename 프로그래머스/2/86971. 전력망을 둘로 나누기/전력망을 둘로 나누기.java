191e199b912f457d65e4412ebbf3b090c7226672
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-13 22:12:38 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.50 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -10,33 +10,36 @@ class Solution {
             link.add(new ArrayList<>(Collections.nCopies(n+1, true)));
             graph.add(new ArrayList<>());
         }
-        for (int[] wire : wires) {
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
+        
+        for (int[] w : wires) {
+            graph.get(w[0]).add(w[1]);
+            graph.get(w[1]).add(w[0]);
         }
-        for (int[] wire : wires) {
+        for (int[] w : wires) {
             visit = new boolean[n+1];
-            link.get(wire[0]).set(wire[1], false);
-            int cnt_a = bfs(wire[0]);
-            int cnt_b = bfs(wire[1]);
-            answer = Math.min(answer, Math.abs(cnt_a-cnt_b));
-            link.get(wire[0]).set(wire[1], true);
+            link.get(w[0]).set(w[1], false);
+            link.get(w[1]).set(w[0], false);
+            int cnt_a = bfs(w[0]);
+            int cnt_b = bfs(w[1]);
+            answer = Math.min(answer, Math.abs(cnt_a - cnt_b));
+            link.get(w[0]).set(w[1], true);
+            link.get(w[1]).set(w[0], true);
         }
         return answer;
     }
     
     static int bfs(int start) {
-        Queue<Integer> que = new LinkedList<>();
+        Queue<Integer> que = new ArrayDeque<>();
         que.add(start);
         visit[start] = true;
         int cnt = 1;
         
         while (!que.isEmpty()) {
             int cur = que.poll();
-            for (int n : graph.get(cur)) {
-                if (!visit[n] && link.get(cur).get(n)) {
-                    visit[n] = true;
-                    que.add(n);
+            for (int a : graph.get(cur)) {
+                if (!visit[a] && link.get(cur).get(a)) {
+                    visit[a] = true;
+                    que.add(a);
                     cnt++;
                 }
             }
