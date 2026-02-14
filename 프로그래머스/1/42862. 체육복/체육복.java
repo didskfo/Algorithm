7739c0bd6902e4c8f668af249828d613bd85e7cf
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-14 12:27:16 +0900

    [level 1] Title: 체육복, Time: 1.92 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,26 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
         int[] stu = new int[n];
-        Arrays.fill(stu, 1);
-        for (int l : lost) {
-            stu[l-1] -= 1;
+        for (int i : lost) {
+            stu[i-1]--;
         }
-        for (int r : reserve) {
-            stu[r-1] += 1;
+        for (int i : reserve) {
+            stu[i-1]++;
         }
         for (int i = 0; i < n; i++) {
-            if (stu[i] == 0) {
-                if (i != 0 && stu[i-1] > 1) {
-                    stu[i] = 1;
-                    stu[i-1] -= 1;
-                } else if (i < n-1 && stu[i+1] > 1) {
-                    stu[i] = 1;
-                    stu[i+1] -= 1;
+            if (stu[i] < 0) {
+                if (i != 0 && stu[i-1] > 0) {
+                    stu[i]++;
+                    stu[i-1]--;
+                } else if (i < n-1 && stu[i+1] > 0) {
+                    stu[i]++;
+                    stu[i+1]--;
                 }
             }
         }
-        return n - (int)Arrays.stream(stu).filter(i -> i == 0).count();
+        return (int)Arrays.stream(stu).filter(i -> i >= 0).count();
     }
 }
