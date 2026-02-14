e89af8c79ec2e785c62b10156f23a989a1e497d0
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-14 22:05:41 +0900

    [level 2] Title: 큰 수 만들기, Time: 143.36 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,19 +1,19 @@
-import java.util.*;
-class Solution {
-    public String solution(String number, int k) {
-        int n = number.length() - k;
-        Stack<Character> st = new Stack<>();
-        for (char a : number.toCharArray()) {
-            while (!st.isEmpty() && k > 0 && st.peek() < a) {
-                st.pop();
-                k--;
-            }
-            st.push(a);
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-             sb.append(st.get(i));
-        }
-        return sb.toString();
+import java.util.*; 
+class Solution { 
+    public String solution(String number, int k) { 
+        int n = number.length() - k; 
+        Stack<Character> st = new Stack<>(); 
+        for (char a : number.toCharArray()) { 
+            while (!st.isEmpty() && k > 0 && st.peek() < a) { 
+                st.pop(); 
+                k--; 
+            } 
+            st.push(a); 
+        } 
+        StringBuilder sb = new StringBuilder(); 
+        for (int i = 0; i < n; i++) { 
+            sb.append(st.get(i)); 
+        } 
+        return sb.toString(); 
     }
 }
