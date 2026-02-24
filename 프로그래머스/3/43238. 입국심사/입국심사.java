53cd86f55d34f9b78926e29789a79621e9d9ac0d
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-24 14:20:44 +0900

    [level 3] Title: 입국심사, Time: 117.51 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,12 +1,13 @@
 import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
         Arrays.sort(times);
         long left = 0;
-        long right = times[times.length-1]*(long)n;
+        long right = (long)times[times.length-1]*n;
         while (left <= right) {
-            long mid = (left+right)/2;
+            long mid = (left + right) / 2;
             long cnt = 0;
             for (int i = 0; i < times.length; i++) {
                 cnt += mid/times[i];
