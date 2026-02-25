e8bc342d0a6eb0e34d39d949c04233d4486da85a
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-25 20:52:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,28 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = Integer.parseInt(sc.nextLine());
-        int ans = 0;
-        if (a % 5 == 0) System.out.println(a/5);
-        else {
-            while (a > 0) {
-                ans += 1;
-                a -= 3;
-                if (a % 5 == 0) {
-                    ans += a/5;
-                    System.out.println(ans);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        if (N % 5 == 0) {
+            System.out.println(N / 5);
+        } else {
+            while (N > 0) {
+                answer++;
+                N -= 3;
+                if (N % 5 == 0) {
+                    answer += N / 5;
+                    System.out.println(answer);
                     break;
-                }
-                if (a == 1 || a == 2) {
+                } 
+                if (N < 3) {
                     System.out.println(-1);
-                    break;
-                }
-                if (a == 0) {
-                    System.out.println(ans);
                     break;
                 }
             }
         }
-	}
+    }
 }
