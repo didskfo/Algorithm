9728412ee9f62ba85c1e1b18f379a87984fee51d
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-27 21:21:56 +0900

    [Silver IV] Title: 스택, Time: 272 ms, Memory: 20500 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,36 +4,29 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 class Main {
-    static Stack<Integer> stack = new Stack<>();
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        for (int t = 0; t < n; t++) {
-            String str = br.readLine();
-            String[] arr = str.split(" ");
-            if (arr[0].equals("push")) push(Integer.parseInt(arr[1]));
-            else if (arr[0].equals("pop")) System.out.println(pop());
-            else if (arr[0].equals("size")) System.out.println(size());
-            else if (arr[0].equals("empty")) System.out.println(empty());
-            else if (arr[0].equals("top")) System.out.println(top());
+        int N = Integer.parseInt(br.readLine());
+        Stack<String> stack = new Stack<>();
+        while (N-- > 0) {
+            String[] input = br.readLine().split(" ");
+            String order = input[0];
+            if (order.equals("push")) {
+                stack.add(input[1]);
+            } else if (order.equals("pop")) {
+                if (stack.size() > 0) {
+                    System.out.println(stack.pop());
+                } else System.out.println(-1);
+            } else if (order.equals("size")) {
+                System.out.println(stack.size());
+            } else if (order.equals("empty")) {
+                if (stack.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else if (order.equals("top")) {
+                if (stack.size() > 0) {
+                    System.out.println(stack.peek());
+                } else System.out.println(-1);
+            }
         }
-    }
-    public static void push(int x) {
-        stack.add(x);
-    }
-    public static int pop() {
-        if (stack.size() != 0) return stack.pop();
-        return -1;
-    }
-    public static int size() {
-        return stack.size();
-    }
-    public static int empty() {
-        if (stack.isEmpty()) return 1;
-        return 0;
-    }
-    public static int top() {
-        if (stack.isEmpty()) return -1;
-        return stack.peek();
     }
 }
