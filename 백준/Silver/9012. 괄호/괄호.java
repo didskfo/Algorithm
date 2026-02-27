3d3ba67a91357d260b5cb6810a3da6306528df64
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-02-27 21:15:35 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,26 +1,27 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        for (int t = 0; t < n; t++) {
-            String str = sc.nextLine();
-            int check = 0;
-            boolean bool = true;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '(') check++;
-                else {
-                    if (check == 0) {
-                        System.out.println("NO");
-                        bool = false;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            char[] str = br.readLine().toCharArray();
+            int a = 0;
+            boolean check = true;
+            for (char c : str) {
+                if (c == '(') {
+                    a++;
+                } else {
+                    if (a <= 0) {
+                        check = false;
                         break;
-                    }
-                    else check--;
+                    } else a--;
                 }
             }
-            if (bool && check != 0) System.out.println("NO");
-            else if (bool && check == 0) System.out.println("YES");
+            if (check == false || a != 0) System.out.println("NO");
+            else System.out.println("YES");
         }
     }
 }
