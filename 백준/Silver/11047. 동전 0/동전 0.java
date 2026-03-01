969f94534aa54a7e0ea0a8449c647f1d3e65d98c
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-01 21:40:15 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int k = Integer.parseInt(str[1]);
-        int[] num = new int[n];
-        for (int i = 0; i < n; i++) {
-            num[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
-        int ans = 0;
-
-        int idx = n-1;
-        while (k != 0) {
-            if (num[idx] <= k) {
-                ans += k/num[idx];
-                k %= num[idx];
-            } 
-            idx--;
+        int answer = 0;
+        for (int i = N-1; i >= 0; i--) {
+            if (coins[i] <= K) {
+                answer += K / coins[i];
+                K -= coins[i] * (K / coins[i]);
+            }
+            if (K == 0) break;
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
