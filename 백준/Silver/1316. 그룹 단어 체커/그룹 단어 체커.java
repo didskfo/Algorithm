df2bc34e613b58109c06011aa421ea94b0e1fc09
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-01 22:54:38 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,29 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int ans = 0;
-        for (int t = 0; t < n; t++) {
-            String str = br.readLine();
-            Stack<Character> st = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+        Stack<Character> stack;
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            char[] str = br.readLine().toCharArray();
             boolean check = true;
-            for (int i = 0; i < str.length(); i++) {
-                char a = str.charAt(i);
-                if (!st.contains(a)) {
-                    st.add(a);
+            stack = new Stack<>();
+            for (char c : str) {
+                if (!stack.contains(c)) {
+                    stack.add(c);
                 } else {
-                    if (st.peek() == a) {
-                        st.add(a);
+                    if (stack.peek() == c) {
+                        stack.add(c);
                     } else {
                         check = false;
+                        break;
                     }
                 }
             }
-            if (check) ans++;
+            if (check) answer++;
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
