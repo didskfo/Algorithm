96b70d8c4e06c379ff6fc22a7b3b6c89accb32e9
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-02 17:41:24 +0900

    [Silver V] Title: 셀프 넘버, Time: 228 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,23 +1,24 @@
-import java.util.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 class Main {
-    public static void main(String[] args) {
-        List<Integer> lst = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <= 10000; i++) {
-            lst.add(i);
+            list.add(i);
         }
         for (int i = 1; i <= 10000; i++) {
-            int self = selfNumber(i);
-            if (lst.contains(self)) {
-                lst.remove(Integer.valueOf(self));
+            int selfNumber = self(i);
+            if (list.contains(selfNumber)) {
+                list.remove(Integer.valueOf(selfNumber));
             }
         }
-        for (int i = 0; i < lst.size(); i++) {
-            System.out.println(lst.get(i));
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
     }
 
-    public static int selfNumber(int a) {
+    static int self(int a) {
         int res = a;
         String str = Integer.toString(a);
         for (int i = 0; i < str.length(); i++) {
