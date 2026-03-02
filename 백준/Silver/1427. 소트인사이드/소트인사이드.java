74f1a9417b56c27d54d4f6d8b3988f87382a8e06
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-02 16:55:08 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,21 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        List<Integer> lst = new ArrayList<>();
-        for (int i = 0; i < input.length(); i++) {
-            lst.add(input.charAt(i)-'0');
-        }
-        Collections.sort(lst);
-        for (int i = lst.size()-1; i >= 0; i--) {
-            System.out.print(lst.get(i));
+        char[] input = br.readLine().toCharArray();
+        Arrays.sort(input);
+        for (int i = input.length-1; i >= 0; i--) {
+            System.out.print(input[i]);
         }
     }
 }
