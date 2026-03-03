9c9a4ff994e62a3707474ef428d45e45cbe73f76
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-03 10:39:40 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -3,25 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int ans = 0;
-        for (int i = 1; i <= n; i++) {
-            String str = Integer.toString(i);
-            boolean check = true;
-            int minus = 0;
-            if (str.length() > 1) {
-                minus = str.charAt(1)-'0' - str.charAt(0)-'0';
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i < 10) answer++;
+            else {
+                if (check(i)) answer++;
             }
-            for (int j = 1; j < str.length()-1; j++) {
-                if (minus != str.charAt(j+1)-'0'-str.charAt(j)-'0') {
-                    check = false;
-                    continue;
-                }
-            }
-            if (check) ans++;
         }
-        System.out.println(ans);
+        System.out.println(answer);
+    }
+
+    static boolean check(int num) {
+        char[] arr = Integer.toString(num).toCharArray();
+        int check = arr[0] - arr[1];
+        for (int i = 1; i < arr.length-1; i++) {
+            if (arr[i] - arr[i+1] != check) return false;
+        }
+        return true;
     }
 }
