e61ea4f7aa9189e49c51d0cabcbb41a34115011c
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-03-16 15:26:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
