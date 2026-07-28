3b6e3787697b666260403a9bca2be275c94ae616
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-07-28 11:23:48 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.86 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,30 +1,42 @@
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int len = cal(video_len);
-        int cur = cal(pos);
-        int start = cal(op_start);
-        int end = cal(op_end);
-        for (String com : commands) {
-            if (cur >= start && cur < end) {
-                cur = end;
-            }
-            if (com.equals("prev")) {
-                cur = Math.max(0, cur-10);
+        String answer = "";
+        int video_len_time = Integer.parseInt(video_len.split(":")[0])*60 + Integer.parseInt(video_len.split(":")[1]);
+        int pos_time = Integer.parseInt(pos.split(":")[0])*60 + Integer.parseInt(pos.split(":")[1]);
+        int op_start_time = Integer.parseInt(op_start.split(":")[0])*60 + Integer.parseInt(op_start.split(":")[1]);
+        int op_end_time = Integer.parseInt(op_end.split(":")[0])*60 + Integer.parseInt(op_end.split(":")[1]);
+        
+        if (pos_time >= op_start_time && pos_time <= op_end_time) {
+            pos_time = op_end_time;
+        }
+        
+        for (String command : commands) {
+            if (command.equals("next")) {
+                pos_time += 10;
+                if (pos_time > video_len_time) pos_time = video_len_time;
+                if (pos_time >= op_start_time && pos_time <= op_end_time) {
+                    pos_time = op_end_time;
+                }
             } else {
-                cur = Math.min(len, cur+10);
+                pos_time -= 10;
+                if (pos_time < 0) pos_time = 0;
+                if (pos_time >= op_start_time && pos_time <= op_end_time) {
+                    pos_time = op_end_time;
+                }
             }
         }
-        if (cur >= start && cur < end) {
-            cur = end;
+        
+        int min = pos_time/60;
+        int sec = pos_time%60;
+        if (min < 10) answer += "0";
+        answer += Integer.toString(min);
+        answer += ":";
+        if (sec == 0) {
+            answer += "00";
+            return answer;
         }
-        int min = cur/60;
-        int sec = cur%60;
-        return String.valueOf(String.format("%02d", min))+":"+String.valueOf(String.format("%02d", sec));
-    }
-    
-    private int cal(String time) {
-        int min = Integer.parseInt(time.split(":")[0]);
-        int sec = Integer.parseInt(time.split(":")[1]);
-        return min*60 + sec;
+        else if (sec < 10) answer += "0";
+        answer += Integer.toString(sec);
+        return answer;
     }
 }
