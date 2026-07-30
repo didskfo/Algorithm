9fdad01134e002d03193d8d7da1e2eb055dc6053
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-07-30 09:51:25 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,29 +1,23 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        if (w == 1) {
-            return n - num + 1;
-        }
-        
-        int targetCol = getColumn(num, w);
-        int count = 0;
-        
+        int answer = 0;
+        int target = getColumn(num, w);
         for (int i = num; i <= n; i++) {
-            if (getColumn(i, w) == targetCol) {
-                count++;
+            if (getColumn(i, w) == target) {
+                answer++;
             }
         }
-        
-        return count;
+        return answer;
     }
     
-    private int getColumn(int i, int w) {
-        int layer      = (i - 1) / w;      
-        int idxInLayer = (i - 1) % w;      
-
+    static int getColumn(int i, int w) {
+        int layer = (i-1)/w;
+        int layerIdx = (i-1)%w;
+        
         if (layer % 2 == 0) {
-            return idxInLayer + 1;        
+            return layerIdx+1;
         } else {
-            return w - idxInLayer;         
+            return w - layerIdx;
         }
     }
 }
