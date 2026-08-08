c4be27a9ec9d891a7cff3761135958235439ba12
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-08-08 14:37:02 +0900

    [level 2] Title: 프로세스, Time: 1.43 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,26 @@
 import java.util.*;
-class Pair {
-    int lo;
-    int pri;
-    public Pair(int lo, int pri) {
-        this.lo = lo;
-        this.pri = pri;
-    }
-}
+
 class Solution {
     public int solution(int[] priorities, int location) {
-        Deque<Pair> que = new ArrayDeque<>();
-        for (int i = 0; i < priorities.length; i++) {
-            que.offer(new Pair(i, priorities[i]));
-        }
         int answer = 0;
-        while (!que.isEmpty()) {
-            boolean flag = true;
-            Pair cur = que.poll();
-            for (Pair p : que) {
-                if (cur.pri < p.pri) {
-                    que.offer(cur);
-                    flag = false;
+        Queue<int[]> que = new ArrayDeque<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int idx = 0;
+        for (int p : priorities) {
+            que.add(new int[]{p, idx++});
+            pq.add(p);
+        }
+        
+        while (true) {
+            int[] cur = que.poll();
+            if (cur[0] < pq.peek()) {
+                que.add(cur);
+            } else {
+                pq.poll();
+                answer++;
+                if (cur[1] == location) {
                     break;
                 }
-            }
-            if (flag) {
-                answer++;
-                if (cur.lo == location) return answer;
             }
         }
         return answer;
