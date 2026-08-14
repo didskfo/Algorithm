fa37688f11a9ad6ff44a0d4d8e7fda7ee486fed8
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-08-14 14:36:12 +0900

    [level 1] Title: 달리기 경주, Time: 205.15 ms, Memory: 367 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,18 +1,22 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        int n = players.length;
+        
         Map<String, Integer> map = new HashMap<>();
-        int len = players.length;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < n; i++) {
             map.put(players[i], i);
         }
+        
         for (String call : callings) {
             int idx = map.get(call);
-            String player = players[idx-1];
+            String tmp = players[idx-1];
             players[idx-1] = call;
-            players[idx] = player;
+            players[idx] = tmp;
+            
             map.put(call, idx-1);
-            map.put(player, idx);
+            map.put(tmp, idx);
         }
         
         return players;
