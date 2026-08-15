511d09d09921d8c24b386c5e36eae5edbff064a9
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-08-15 13:07:06 +0900

    [level 2] Title: 주차 요금 계산, Time: 3.88 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,40 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        Map<String, Integer> in = new HashMap<>();
-        Map<String, Integer> total = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> time = new HashMap<>();
+        
         for (String record : records) {
-            String[] str = record.split(" ");
-            String car = str[1];
-            int min = Integer.parseInt(str[0].split(":")[0]) * 60 + Integer.parseInt(str[0].split(":")[1]);
-            if (str[2].equals("IN")) {
-                in.put(car, min);
+            String[] lst = record.split(" ");
+            if (lst[2].equals("IN")) {
+                map.put(lst[1], toMinute(lst[0]));
             } else {
-                int time = in.remove(car);
-                total.put(car, total.getOrDefault(car, 0) + min-time);
+                int start = map.remove(lst[1]);
+                int end = toMinute(lst[0]);
+                time.put(lst[1], time.getOrDefault(lst[1], 0)+end-start);
             }
         }
-        int end = 23 * 60 + 59;
-        for (Map.Entry<String, Integer> e : in.entrySet()) {
-            String car = e.getKey();
-            int start = e.getValue();
-            total.put(car, total.getOrDefault(car, 0) + (end - start));
+        
+        int end = 23*60+59;
+        
+        for (String number : map.keySet()) {
+            int start = map.get(number);
+            time.put(number, time.getOrDefault(number, 0)+end-start);
         }
         
-        List<String> cars = new ArrayList<>(total.keySet());
+        List<String> cars = new ArrayList<>(time.keySet());
         Collections.sort(cars);
+        
         int[] answer = new int[cars.size()];
         int idx = 0;
         for (String car : cars) {
-            int minutes = total.get(car);
-            int fee = fees[1];
-            if (minutes > fees[0]) {
-                int extra = minutes - fees[0];
-                int units = (extra + fees[2] - 1) / fees[2];
-                fee += units * fees[3];
+            int min = time.get(car);
+            if (min < fees[0]) {
+                answer[idx++] = fees[1];
+            } else {
+                min -= fees[0];
+                answer[idx++] = fees[1] + (min + fees[2] - 1)/fees[2]*fees[3];
             }
-            answer[idx++] = fee;
         }
         return answer;
+    }
+    
+    static int toMinute(String str) {
+        String[] lst = str.split(":");
+        return Integer.parseInt(lst[0])*60 + Integer.parseInt(lst[1]);
     }
 }
