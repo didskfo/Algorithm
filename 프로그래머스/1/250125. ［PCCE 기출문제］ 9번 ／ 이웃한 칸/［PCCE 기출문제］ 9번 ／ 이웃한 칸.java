d1c95336c4809e7cc465cf3cfd2ed3f3b8cc7c1c
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-08-21 11:08:27 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -1,14 +1,18 @@
+import java.util.*;
+
 class Solution {
     static int[] dx = {-1, 0, 0, 1};
     static int[] dy = {0, -1, 1, 0};
     
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
+        int n = board.length;
         String color = board[h][w];
+        
         for (int i = 0; i < 4; i++) {
             int nx = h+dx[i];
             int ny = w+dy[i];
-            if (nx>=0&&nx<board.length&&ny>=0&&ny<board[0].length&&board[nx][ny].equals(color)) answer++; 
+            if (nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny].equals(color)) answer++;
         }
         return answer;
     }
