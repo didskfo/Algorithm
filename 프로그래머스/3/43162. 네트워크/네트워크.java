579db80ee2612c34f1c44dd68a4f0f594f594956
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-02 10:18:38 +0900

    [level 3] Title: 네트워크, Time: 1.53 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,19 +3,23 @@ import java.util.*;
 class Solution {
     static boolean[] visit;
     static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+        
         for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
+            for (int j = 0; j < computers[0].length; j++) {
                 if (computers[i][j] == 1) {
                     graph.get(i).add(j);
                     graph.get(j).add(i);
                 }
             }
         }
+        
         visit = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (!visit[i]) {
@@ -23,13 +27,14 @@ class Solution {
                 bfs(i);
             }
         }
+        
         return answer;
     }
     
     static void bfs(int start) {
         visit[start] = true;
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
+        Queue<Integer> que = new ArrayDeque<>();
+        que.offer(start);
         while (!que.isEmpty()) {
             int cur = que.poll();
             for (int a : graph.get(cur)) {
