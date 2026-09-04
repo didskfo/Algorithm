a2e61b2136ec94aed1ea12d4b85b7ce38da382b3
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-04 09:37:04 +0900

    [level 3] Title: 단어 변환, Time: 0.11 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,38 +2,39 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        if (!Arrays.asList(words).contains(target)) return 0;
-        int[] visited = new int[words.length];
-        Deque<Integer> que = new ArrayDeque<>();
+        int[] visit = new int[words.length];
+        Queue<Integer> que = new ArrayDeque<>();
         
         for (int i = 0; i < words.length; i++) {
-            if (visited[i] == 0 && diff(begin, words[i])) {
+            if (visit[i] == 0 && diff(begin, words[i])) {
                 que.offer(i);
-                visited[i] = 1;
+                visit[i] = 1;
             }
         }
         
         while (!que.isEmpty()) {
             int cur = que.poll();
             String str = words[cur];
-            if (str.equals(target)) return visited[cur];
+            
+            if (str.equals(target)) return visit[cur];
+            
             for (int i = 0; i < words.length; i++) {
-                if (visited[i] == 0 && diff(str, words[i])) {
+                if (visit[i] == 0 && diff(str, words[i])) {
                     que.offer(i);
-                    visited[i] = visited[cur]+1;
+                    visit[i] = visit[cur]+1;
                 }
             }
         }
-        return answer;
+        return 0;
     }
     
-    static boolean diff(String a, String b) {
+    static boolean diff(String str1, String str2) {
         int cnt = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) cnt++;
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) != str2.charAt(i)) cnt++;
             if (cnt > 1) return false;
         }
+        
         return cnt == 1;
     }
 }
