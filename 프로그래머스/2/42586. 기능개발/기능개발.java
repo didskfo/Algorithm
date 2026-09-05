b51ee7675de338d2c7fc7cde5442196485ae27c2
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-05 12:27:06 +0900

    [level 2] Title: 기능개발, Time: 2.83 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int day = 0; 
         int[] res = new int[100];
+        int day = 1;
         for (int i = 0; i < progresses.length; i++) {
-            while (progresses[i] + day*speeds[i] < 100) {
+            while (progresses[i] + speeds[i]*day < 100) {
                 day++;
             }
             res[day]++;
         }
-        
         return Arrays.stream(res).filter(i -> i != 0).toArray();
     }
 }
