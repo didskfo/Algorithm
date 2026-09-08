7bb3723e79f709dd3775f89272cefe4251061c47
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-08 17:59:02 +0900

    [level 3] Title: 이중우선순위큐, Time: 55.68 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        int[] answer = new int[2];
         PriorityQueue<Integer> que1 = new PriorityQueue<>();
         PriorityQueue<Integer> que2 = new PriorityQueue<>(Collections.reverseOrder());
-        for (String operation : operations) {
-            String[] str = operation.split(" ");
+        for (String oper : operations) {
+            String[] str = oper.split(" ");
             if (str[0].equals("I")) {
                 que1.offer(Integer.parseInt(str[1]));
                 que2.offer(Integer.parseInt(str[1]));
+            } else if (str[1].equals("1")) {
+                que1.remove(que2.poll());
             } else {
-                if (!que1.isEmpty() && Integer.parseInt(str[1]) == 1) {
-                    int max = que2.poll();
-                    que1.remove(max);
-                } else if (!que1.isEmpty()){
-                    int min = que1.poll();
-                    que2.remove(min);
-                }
+                que2.remove(que1.poll());
             }
         }
+        
         if (que1.isEmpty()) return new int[]{0, 0};
-        else return new int[]{que2.poll(), que1.poll()};
+        answer[0] = que2.peek();
+        answer[1] = que1.peek();
+        return answer;
     }
 }
