caa04408b8396ef08dd3e004d1bc666a5aa7ca34
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-11 17:47:54 +0900

    [level 3] Title: 단속카메라, Time: 8.49 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        Arrays.sort(routes, (r1, r2) -> Integer.compare(r1[1], r2[1]));
-        int end = Integer.MIN_VALUE;
+        Arrays.sort(routes, (r1, r2) -> r1[1] - r2[1]);
+        int end = -30001;
         for (int[] route : routes) {
             if (end < route[0]) {
-                answer++;
                 end = route[1];
+                answer++;
             }
         }
         return answer;
