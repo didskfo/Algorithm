c034685f60bed60989cd2555745d56c4191ecb1f
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-14 19:29:33 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -4,14 +4,12 @@ public class Solution {
     public int solution(int n) {
         int ans = 0;
         while (n > 0) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
+            if (n % 2 == 0) n /= 2;
+            else {
                 n -= 1;
                 ans++;
             }
         }
-
         return ans;
     }
 }
