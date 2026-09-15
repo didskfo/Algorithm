26bee761607bdac4a4416231745fd8b02994713d
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-15 14:26:15 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,23 +1,20 @@
 class Solution {
     public int solution(int[] arr) {
         if (arr.length == 1) return arr[0];
-        int answer = lcm(arr[0], arr[1]);
-        for (int i = 2; i < arr.length; i++) {
-            answer = lcm(answer, arr[i]);
+        int answer = 1;
+        for (int i = 0; i < arr.length; i++) {
+            answer = answer * arr[i] / gcd(answer, arr[i]);
         }
         return answer;
     }
     
-    private int gcd(int a, int b) {
+    int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
-            b = a % b;
+            b = a%b;
             a = temp;
         }
+        
         return a;
-    }
-    
-    private int lcm(int a, int b) {
-        return a * b / gcd(a, b);
     }
 }
