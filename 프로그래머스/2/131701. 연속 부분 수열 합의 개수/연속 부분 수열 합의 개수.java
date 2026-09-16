5fd3d300639c3c7020866cbdbdda23e6824246af
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-16 12:56:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 54.62 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,23 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
+        Set<Integer> res = new HashSet<>();
         int len = elements.length;
-        int[] circle = new int[2*len];
-        System.arraycopy(elements, 0, circle, 0, len);
-        System.arraycopy(elements, 0, circle, len, len);
-
-        int[] dp = new int[2 * len + 1];
-        for (int i = 0; i < 2 * len; i++) {
+        int[] circle = new int[len*2];
+        for (int i = 0; i < len; i++) {
+            circle[i] = elements[i];
+            circle[i+len] = elements[i];
+        }
+        
+        int[] dp = new int[2*len+1];
+        for (int i = 0; i < 2*len; i++) {
             dp[i+1] = dp[i] + circle[i];
         }
         
-        Set<Integer> set = new HashSet<>();
-        
         for (int k = 1; k <= len; k++) {
             for (int i = 0; i < len; i++) {
-                set.add(dp[i + k] - dp[i]);
+                res.add(dp[i+k] - dp[i]);
             }
         }
-        return set.size();
+        return res.size();
     }
 }
