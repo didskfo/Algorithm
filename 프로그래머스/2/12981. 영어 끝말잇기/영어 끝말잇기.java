0146d8e13a1b48427dc44825b10cf3bde7222741
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-17 13:40:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.12 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,16 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {0, 0};
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < words.length; i++) {
-            if ((i > 0 && words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) || map.getOrDefault(words[i], 0) != 0) {
+        int[] answer = new int[2];
+        ArrayList<String> use = new ArrayList<>();
+        use.add(words[0]);
+        
+        for (int i = 1; i < words.length; i++) {
+            if (words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
                 answer[0] = i%n+1;
                 answer[1] = i/n+1;
                 break;
+            } else if (use.contains(words[i])) {
+                answer[0] = i%n+1;
+                answer[1] = i/n+1;
+                break;
+            } else {
+                use.add(words[i]);
             }
-            map.put(words[i], 1);
         }
+
         return answer;
     }
 }
