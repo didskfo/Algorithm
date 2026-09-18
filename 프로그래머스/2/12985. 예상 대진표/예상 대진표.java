222474acb875ac659a7e8888f8ff70b9d50b9deb
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-18 16:10:54 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,12 +2,13 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1;
-        while (!(Math.abs(a-b) == 1 && Math.min(a, b)%2 == 1)) {
-            a = a%2==1?(a+1)/2:a/2;
-            b = b%2==1?(b+1)/2:b/2;
+        int answer = 0;
+        while (a != b) {
+            a = (a+1)/2;
+            b = (b+1)/2;
             answer++;
         }
+
         return answer;
     }
 }
