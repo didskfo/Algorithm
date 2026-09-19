a75aa199b38be213050b59bdcd35af7a5b781099
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-19 16:21:38 +0900

    [level 2] Title: 할인 행사, Time: 15.84 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,21 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put(discount[i], map.getOrDefault(discount[i], 0)+1);
+        }
+        
         for (int i = 0; i <= discount.length-10; i++) {
-            int[] num = number.clone();
-            boolean check = true;
-            for (int j = 0; j < 10; j++) {
-                int idx = Arrays.asList(want).indexOf(discount[i+j]);
-                if (idx != -1) num[idx]--;
+            if (check(want, number, map)) {
+                answer++;
             }
-            for (int n : num) {
-                if (n != 0) {
-                    check = false;
-                }
+            
+            if (i != discount.length-10) {
+                map.put(discount[i], map.get(discount[i])-1);
+                map.put(discount[i+10], map.getOrDefault(discount[i+10], 0)+1);
             }
-            if (check) answer++;
         }
         return answer;
+    }
+    
+    boolean check(String[] want, int[] number, HashMap<String, Integer> map) {
+        for (int i = 0; i < want.length; i++) {
+            if (map.getOrDefault(want[i], 0) != number[i]) return false;
+        }
+        return true;
     }
 }
