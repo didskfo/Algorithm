8db262304b3906a244aa2e1bd241f073bdc68ee3
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-19 16:49:05 +0900

    [level 3] Title: 섬 연결하기, Time: 0.66 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,15 +4,18 @@ class Solution {
     static int[] parent;
     public int solution(int n, int[][] costs) {
         int answer = 0;
+        Arrays.sort(costs, (c1, c2) -> c1[2] - c2[2]);
+        
         parent = new int[n];
         for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
-        Arrays.sort(costs, (c1, c2) -> c1[2] - c2[2]);
+        
         for (int[] cost : costs) {
             int a = cost[0];
             int b = cost[1];
             int c = cost[2];
+            
             if (find(a) != find(b)) {
                 union(a, b);
                 answer += c;
@@ -29,6 +32,7 @@ class Solution {
     static void union(int a, int b) {
         a = find(a);
         b = find(b);
+        
         if (a > b) parent[a] = b;
         else parent[b] = a;
     }
