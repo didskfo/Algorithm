449d26edc9f8dedc28e9cfeda8a30dc7a26a0665
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-21 17:01:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 20.38 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,41 +1,35 @@
 import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String str = s+s;
+        String s2 = s+s;
         for (int i = 0; i < s.length(); i++) {
             Deque<Character> que = new ArrayDeque<>();
             boolean check = true;
             for (int j = 0; j < s.length(); j++) {
-                char c = str.charAt(i+j);
+                char c = s2.charAt(i+j);
                 if (que.isEmpty()) {
-                    que.add(c);
+                    que.offer(c);
                 } else if (c == '(' || c == '[' || c == '{') {
-                    que.add(c);
-                } else if (c == ')') {
-                    if (que.peekLast() != '(') {
-                        check = false;
-                        break;
-                    } else {
-                        que.removeLast();
-                    }
-                } else if (c == ']') {
-                    if (que.peekLast() != '[') {
-                        check = false;
-                        break;
-                    } else {
-                        que.removeLast();
-                    }
-                } else if (c == '}') {
-                    if (que.peekLast() != '{') {
-                        check = false;
-                        break;
-                    } else {
-                        que.removeLast();
-                    }
+                    que.offer(c);
+                } else if (c == ')' && que.peekLast() != '(') {
+                    check = false;
+                    break;
+                } else if (c == ']' && que.peekLast() != '[') {
+                    check = false;
+                    break;
+                } else if (c == '}' && que.peekLast() != '{') {
+                    check = false;
+                    break;
+                } else {
+                    que.removeLast();
                 }
             }
-            if (check && que.isEmpty()) answer++;
+            
+            if (check && que.isEmpty()) {
+                answer++;
+            }
         }
         return answer;
     }
