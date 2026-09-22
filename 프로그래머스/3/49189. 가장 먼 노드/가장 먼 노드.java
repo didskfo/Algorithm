171e491c47a4f4f261e521de948181014af1e87e
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-22 11:18:28 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.67 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,32 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        int max = 0;
+        int answer = 0;
         int[] dist = new int[n+1];
+        int maxDist = 0;
+        
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
+        
         for (int[] ed : edge) {
             graph.get(ed[0]).add(ed[1]);
             graph.get(ed[1]).add(ed[0]);
         }
+        
         Deque<Integer> que = new ArrayDeque<>();
         que.offer(1);
+        
         while (!que.isEmpty()) {
             int cur = que.poll();
             for (int a : graph.get(cur)) {
                 if (dist[a] == 0) {
-                    dist[a] = dist[cur]+1;
-                    max = Math.max(max, dist[a]);
+                    dist[a] = dist[cur] + 1;
+                    maxDist = Math.max(maxDist, dist[a]);
                     que.offer(a);
                 }
             }
         }
-        int answer = 0;
+        
         for (int i = 2; i <= n; i++) {
-            if (dist[i] == max) answer++;
+            if (dist[i] == maxDist) answer++;
         }
-        return answer;    
+        return answer;
     }
 }
