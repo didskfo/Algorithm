e3b59d57587116920a5f3de13488baf7aabb3248
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-23 12:43:24 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,6 +4,7 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         HashMap<String, Integer> map = new HashMap<>();
+        
         for (String[] cloth : clothes) {
             map.put(cloth[1], map.getOrDefault(cloth[1], 0)+1);
         }
