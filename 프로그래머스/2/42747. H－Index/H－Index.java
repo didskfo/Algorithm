286990fce438cc665efc90e20a2506405b7533f7
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-24 22:45:34 +0900

    [level 2] Title: H-Index, Time: 3.49 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
+        
         for (int i = 0; i < citations.length; i++) {
             if (i+1 >= citations[citations.length-i-1]) {
                 return Math.max(i, citations[citations.length-i-1]);
