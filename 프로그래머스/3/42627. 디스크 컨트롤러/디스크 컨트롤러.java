fdeec8b3eddd34d9c5dfd66bf4b6cbcad7410cde
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-25 21:59:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.67 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,23 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
         int answer = 0;
-        int idx = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a[1], b[1])
+        );
+        int count = 0;
         int before = -1;
         int time = 0;
-        while (idx < jobs.length) {
+        
+        while (count < jobs.length) {
             for (int[] job : jobs) {
                 if (job[0] <= time && job[0] > before) {
-                    que.add(job);
+                    pq.add(job);
                 }
             }
-            if (!que.isEmpty()) {
-                int[] cur = que.poll();
+            
+            if (!pq.isEmpty()) {
+                int[] cur = pq.poll();
                 before = time;
                 time += cur[1];
                 answer += time-cur[0];
-                idx++;
+                count++;
             } else {
                 time++;
             }
