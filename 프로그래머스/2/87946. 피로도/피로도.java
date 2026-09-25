0c8d4401c2fcc423597be262a7f97d3cd8b0c5b2
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-25 19:45:29 +0900

    [level 2] Title: 피로도, Time: 2.59 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,15 +1,17 @@
+import java.util.*;
+
 class Solution {
     static boolean[] visit;
     static int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        visit = new boolean[dungeons.length];
+        visit = new boolean[dungeons.length];        
         dfs(k, 0, dungeons);
         return answer;
     }
     
     static void dfs(int tired, int cnt, int[][] dungeons) {
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visit[i] && dungeons[i][0] <= tired) {
+            if (!visit[i] && tired >= dungeons[i][0]) {
                 visit[i] = true;
                 dfs(tired-dungeons[i][1], cnt+1, dungeons);
                 visit[i] = false;
