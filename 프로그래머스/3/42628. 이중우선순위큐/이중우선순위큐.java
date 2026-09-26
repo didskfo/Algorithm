f03591ed532900c486330762c42b6f24f1bd54c1
Narae Yang <101495452+didskfo@users.noreply.github.com>
2026-09-26 12:39:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 73.49 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,24 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        PriorityQueue<Integer> que1 = new PriorityQueue<>();
-        PriorityQueue<Integer> que2 = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
         for (String oper : operations) {
             String[] str = oper.split(" ");
             if (str[0].equals("I")) {
-                que1.offer(Integer.parseInt(str[1]));
-                que2.offer(Integer.parseInt(str[1]));
-            } else if (str[1].equals("1")) {
-                que1.remove(que2.poll());
-            } else {
-                que2.remove(que1.poll());
+                pq1.offer(Integer.parseInt(str[1]));
+                pq2.offer(Integer.parseInt(str[1]));
+            } else if (str[1].equals("1") && !pq1.isEmpty()) {
+                pq1.remove(pq2.poll());
+            } else if (str[1].equals("-1") && !pq1.isEmpty()) {
+                pq2.remove(pq1.poll());
             }
         }
         
-        if (que1.isEmpty()) return new int[]{0, 0};
-        answer[0] = que2.peek();
-        answer[1] = que1.peek();
+        int[] answer = new int[2];
+        if (pq1.isEmpty()) {
+            answer[0] = 0;
+            answer[1] = 0;
+        } else {
+            answer[0] = pq2.poll();
+            answer[1] = pq1.poll();
+        }
         return answer;
     }
 }
